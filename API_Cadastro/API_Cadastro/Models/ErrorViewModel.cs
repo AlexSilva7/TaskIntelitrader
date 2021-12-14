f47a7296fd3b051a295e46edadfffbce190a6134
Alex Silva <alexaraujo_rj@yahoo.com.br>
2021-12-14 11:22:31 -0300

    Estrutura

---
+++ API_Cadastro/API_Cadastro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace API_Cadastro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
