db0150e2fab6630beb28036fb2143348482281f5
Alex Silva <alexsaraujo92@gmail.com>
2021-12-15 20:29:33 -0300

    conexao DB

---
+++ API_Cadastro/API_Cadastro/Data/UsuarioDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using API_Cadastro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Cadastro.Data
+{
+    public class UsuarioDbContext : DbContext
+    {
+        public UsuarioDbContext(DbContextOptions<UsuarioDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
