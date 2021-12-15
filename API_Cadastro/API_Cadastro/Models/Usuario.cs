160260b369f5fea3074398a5ead159aa4472fcfa
Alex Silva <alexsaraujo92@gmail.com>
2021-12-15 20:28:07 -0300

    classe Modelo

---
+++ API_Cadastro/API_Cadastro/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Cadastro.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public string ID { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string? Surname { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+    }
+}
+
