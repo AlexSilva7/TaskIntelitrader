8d2896e7f8eaae71fd86633531c45b6a1e676eda
Alex Silva <alexsaraujo92@gmail.com>
2021-12-15 20:30:31 -0300

    Ajustes Main

--- API_Cadastro/API_Cadastro/Program.cs
+++ API_Cadastro/API_Cadastro/Program.cs
@@ -1,8 +1,16 @@
+using API_Cadastro.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<UsuarioDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
+//builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
