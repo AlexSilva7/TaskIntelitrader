c9b841ef70e809fe445b0b708c972a40a9546d00
Alex Silva <alexsaraujo92@gmail.com>
2021-12-16 18:50:59 -0300

    add class user

---
+++ API_Cadastro/API_Cadastro/Controllers/UserController.cs
@@ -0,0 +1,108 @@
+﻿using API_Cadastro.Data;
+using API_Cadastro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Cadastro.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserDbContext _db;
+
+        public UserController(UserDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("Users/")]
+        public IActionResult Index()
+        {
+            IEnumerable<Usuario> objUsuarioList = _db.Users.ToList();
+
+            return Ok(objUsuarioList);
+        }
+
+
+        [HttpPost("Users/")]
+        public IActionResult Create([FromBody] Usuario obj)
+        {
+
+            if (obj == null)
+            {
+                return BadRequest("Objeto invalido!");
+            }
+
+            if(obj.FirstName == null || obj.FirstName == "")
+            {
+                return BadRequest("Objeto invalido!");
+            }
+
+            if (obj.Age <= 0)
+            {
+                return BadRequest("Objeto invalido!");
+            }
+            
+            _db.Users.Add(obj);
+            _db.SaveChanges();
+            return Ok("Usuario adicionado com sucesso!");
+
+        }
+
+
+        [HttpPut("Users/{id}")]
+        public IActionResult Update(string id, [FromBody] Usuario obj)
+        {
+
+            if (id == null || id == "")
+            {
+                return BadRequest("Objeto invalido!");
+            }
+
+            if (obj.Age <= 0)
+            {
+                return BadRequest("Objeto invalido!");
+            }
+
+            if (obj == null)
+            {
+                return BadRequest("Objeto invalido!");
+            }
+
+            var userFromDb = _db.Users.Find(id);
+            
+            userFromDb.FirstName = obj.FirstName;
+            userFromDb.Surname = obj.Surname;
+            userFromDb.Age = obj.Age;
+            
+            _db.Users.Update(userFromDb);
+            _db.SaveChanges();
+            return Ok("Usuario atualizado com sucesso!");
+
+        }
+        
+
+        [HttpDelete("Users/{id}")]
+        public IActionResult Delete(string id)
+        {
+            if (id == null || id == "")
+            {
+                //return NotFound();
+                return BadRequest("Objeto invalido!");
+            }
+
+            var userFromDb = _db.Users.Find(id);
+
+            if (userFromDb == null)
+            {
+                //return NotFound();
+                return BadRequest("Erro: Usuario nao encontrado!");
+            }
+
+            _db.Users.Remove(userFromDb);
+            _db.SaveChanges();
+
+            //mostrar alteracao
+            return Ok("Usuario deletado com sucesso!");
+        }
+
+    }
+}
