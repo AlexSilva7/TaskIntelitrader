c758bd8d4d5450ead1f49cce12086938ca15e8fb
Alex Silva <alexsaraujo92@gmail.com>
2021-12-16 18:57:07 -0300

    Comenta classe anterior

--- API_Cadastro/API_Cadastro/Models/Usuario.cs
+++ API_Cadastro/API_Cadastro/Models/Usuario.cs
@@ -6,17 +6,41 @@ namespace API_Cadastro.Models
     {
         [Key]
         public string ID { get; set; } = Guid.NewGuid().ToString();
-
+        
         [Required]
         public string FirstName { get; set; }
-
+        /*
+         * [FirstName("Primeiro Nome")]
+         * especifica como deve aparecer no front
+         */
         public string? Surname { get; set; }
 
         [Required]
         public int Age { get; set; }
+        /*
+         * [Range(1,100,ErrorMessage="Idade deve ser entre 1 e 150!!")]
+         * exemplo de erro criado na classe
+         */
 
         [Required]
         public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        
+        /*
+        public Usuario(string name, string? surname, int age)
+        {
+            FirstName = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public Usuario(string name, int age)
+        {
+            FirstName = name;
+            Age = age;
+        }
+        */
+
     }
 }
 
