42c331bc49d2864f72b9dc19362aa5964e4dd493
Alex Silva <alexsaraujo92@gmail.com>
2021-12-16 18:54:10 -0300

    atualiza db context

---
+++ API_Cadastro/API_Cadastro/Data/UserDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using API_Cadastro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Cadastro.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Users { get; set; }
+
+    }
+}
